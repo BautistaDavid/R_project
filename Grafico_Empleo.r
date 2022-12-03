0d58569365eb396c1a2e7897e3b0967aafc47fdf
David Felipe Bautista <78710558+BautistaDavid@users.noreply.github.com>
2022-12-02 22:19:21 -0500

    Update Grafico_Empleo.r

--- Grafico_Empleo.r
+++ Grafico_Empleo.r
@@ -1,7 +1,10 @@
 library(tidyverse)
 library(ggplot2)
 
+# Se importan datos
 df <- read_delim('empleo.csv',delim=';')
+
+# Se Modifican los datos 
 df <- df %>% mutate(año = substr(periodo, start = 1, stop = 4),
               mes = substr(periodo, start = 6, stop = 7))
 
@@ -50,48 +53,7 @@ label_data <- label_data %>%
 data <- data %>% 
   mutate(value_round = round(value,2))
 
-
-
-
-
-
-
-# Start the plot
-p <- ggplot(data, aes(x=as.factor(id), y=value)) +       # Note that id is a factor. If x is numeric, there is some space between the first bar
-  
-  # This add the bars with a blue color
-  geom_bar(stat="identity", fill=alpha("skyblue", 0.7)) +
-  
-  # Limits of the plot = very important. The negative value controls the size of the inner circle, the positive one is useful to add size over each bar
-  ylim(-8,18) +
-  
-  # Custom the theme: no axis title and no cartesian grid
-  theme_minimal() +
-  theme(
-    axis.text = element_blank(),
-    axis.title = element_blank(),
-    panel.grid = element_blank(),
-    plot.margin = margin(1,1,1,1,)      # Adjust the margin to make in sort labels are not truncated!
-  ) +
-  
-
-  
-  # This makes the coordinate polar instead of cartesian.
-  coord_polar(start = 0) +
-  
-  # Add the labels, using the label_data dataframe that we have created before
-  geom_text(data=label_data, aes(x=id, y=value+1, label=individual, hjust=hjust), color="black", fontface="bold",alpha=0.6, size=2.5, angle= label_data$angle, inherit.aes = FALSE )+
-  geom_text(
-    x = 0, y = -8,
-    label = "********\nPorcentaje Promedio\nde Ofertas Laborales\n en El Tiempo 1976 - 2022",
-    family = "Roboto Mono Medium",
-    size = 4,
-    lineheight = 0.87,
-    color = "grey60")
-
-p
-
-
+# Grafico
 
 fig1 <- ggplot(data, aes(x=as.factor(id), y=value, fill = value)) +
   geom_bar(stat="identity") +
@@ -128,10 +90,6 @@ fig1 <- ggplot(data, aes(x=as.factor(id), y=value, fill = value)) +
     fontface="bold") + 
   coord_polar(start=0) 
   
-
-  
-fig1
-
-
+# Guardamos Figura
 ggsave("circle_test.png", width = 10, height = 10, units = "in", dpi = 300)
   
